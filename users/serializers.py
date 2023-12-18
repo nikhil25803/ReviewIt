27d199ed6e899e1d068254415c3a88adaf438512
Nikhil Raj <nikhil25803@gmail.com>
2023-12-18 21:13:46 +0530

    user authentication flow

---
+++ users/serializers.py
@@ -0,0 +1,57 @@
+from rest_framework import serializers
+from .models import UserModel
+
+
+"""User Registration Serializer"""
+
+
+class UserRegistrationSerializer(serializers.Serializer):
+    uid = serializers.CharField()
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    name = serializers.CharField()
+
+    # Validate incoming data for registration
+    def validate(self, data):
+        # Validate UID
+        if data["uid"]:
+            if UserModel.objects.filter(uid=data["uid"]).exists():
+                raise serializers.ValidationError(detail="UID provided already exists")
+
+        # Validate Username
+        if data["username"]:
+            if UserModel.objects.filter(username=data["username"]).exists():
+                raise serializers.ValidationError(
+                    detail="User with same username exists"
+                )
+
+        # Validate Email
+        if data["email"]:
+            if UserModel.objects.filter(email=data["email"]).exists():
+                raise serializers.ValidationError(
+                    detail="This email is already registered"
+                )
+
+        return data
+
+    # Create a new user object if all the provided data is valid
+    def create(self, data):
+        new_user = UserModel.objects.create(
+            uid=data["uid"],
+            username=data["username"],
+            email=data["email"],
+            name=data["name"],
+        )
+
+        new_user.save()
+        return new_user
+
+
+"""User Login Serializer"""
+
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = "__all__"
+        # exclude = ["uid"]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import UserModel
+from .serializers import UserRegistrationSerializer, UserLoginSerializer
+from rest_framework import status
 
-# Create your views here.
+
+"""User API Ping Test"""
+
+
+@api_view(http_method_names=["GET"])
+def user_index(request):
+    details = {
+        "status": 200,
+        "message": "Project ReviewIt",
+        "github": "https://github.com/nikhil25803/ReviewIt",
+    }
+    return JsonResponse(data=details)
+
+
+"""User API - User Authentication (For both registration and validation)"""
+
+
+class UserAuthentication(APIView):
+    # Method POST - For Registration/Login
+    def post(self, request):
+        incoming_data = request.data
+
+        # If user exists - Login Else - Register
+        if UserModel.objects.filter(uid=incoming_data["uid"]).exists():
+            user_object = UserModel.objects.filter(uid=incoming_data["uid"]).first()
+            serialied_data = UserLoginSerializer(user_object)
+            if serialied_data:
+                return Response(data=serialied_data.data, status=status.HTTP_200_OK)
+
+            return Response(
+                {
+                    "status": status.HTTP_400_BAD_REQUEST,
+                    "message": serialied_data.errors,
+                }
+            )
+
+        else:
+            validation = UserRegistrationSerializer(data=incoming_data)
+            if validation.is_valid(raise_exception=True):
+                validation.save()
+                return Response(
+                    {
+                        "status": status.HTTP_202_ACCEPTED,
+                        "message": "User has been registered",
+                    }
+                )
+            return Response(
+                {"status": status.HTTP_400_BAD_REQUEST, "message": validation.errors}
+            )
