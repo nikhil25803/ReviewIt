206dc994fd2618220b734f60ec27a1c7a9da790b
Nikhil Raj <nikhil25803@gmail.com>
2023-12-22 21:05:47 +0530

    configured all pages

--- frontend/src/components/userProfile/ProfileResponse.jsx
+++ frontend/src/components/userProfile/ProfileResponse.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ResponseCard from "../cards/ResponseCard";
 
-const ProfileResponse = () => {
+const ProfileResponse = (props) => {
+  // Collecting Props
+  const data = props.props;
+
+  // Getting the requests data
+  const responseData = data.response;
   return (
     <div>
       <div className="bg-backgroundDark mt-10 p-4 text-white font-poppins rounded-lg">
@@ -9,7 +14,23 @@ const ProfileResponse = () => {
           Your Responses <span className="text-textLight">...</span>
         </div>
         <div className="mt-5 h-fit grid grid-cols-1 md:grid-cols-2 gap-4 rounded-lg">
-          <ResponseCard />
+          {responseData.length >= 1 ? (
+            responseData.map((res) => {
+              let resData = {
+                avatar: res.avatar,
+                responsemessage: res.responsemessage,
+                email: res.email,
+                requestedat: res.requestedat,
+                resumelink: res.resumelink,
+                name: res.name,
+              };
+              return <ResponseCard key={res.id} props={resData} />;
+            })
+          ) : (
+            <div className="flex flex-row justify-center items-center">
+              <h1>No response available</h1>
+            </div>
+          )}
         </div>
       </div>
     </div>
